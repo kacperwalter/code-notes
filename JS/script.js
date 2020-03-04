2288b3dd4b57a9b48dc4da77c59c7948aaf8a3b0
Kacper <kacwal@poczta.fm>
2020-03-04 21:14:11 +0100

    js

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+console.log("elo skurwysyny");
+
+// ZMIENNE
+// camel case <= konwencja
+var mojaZmienna1 = 1;
+let mojaZmienna2 = 1;
+const mojaStala = 1;
+
+// zmienia int na string xd
+let a = 3;
+let b = "2";
+let c = a+b;
+
+a++; // inkrementacja dziala normalnie
+a--; // dekrementacja tez
+
+ a += 1; // dziala jak w python
+
+
+
+//  CONDITIONALS syntax
+
+if (a == b) {
+    console.log('true');
+} else {
+    console.log('false');
+}
