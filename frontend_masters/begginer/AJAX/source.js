3da241ece8292fbc7c1b88c90f211ad078ebe8d5
Kacper <kacwal@poczta.fm>
2020-06-09 14:57:41 +0200

    integrating libraries

--- frontend_masters/begginer/AJAX/source.js
+++ frontend_masters/begginer/AJAX/source.js
@@ -1,14 +1,20 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
-const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".doggos");
 
-promise //function runs whenever promis is resolved - needs time to back from server (this is async)
-  .then(function(response) { // it comes as a block of text to resolve it
-    const processingPromise = response.json(); // parsing to object i can use (giving another promis)
-    return processingPromise; // returning another promise (promise chaining)
-  })
-  .then(function(processedResponse) {
-    console.log(processedResponse);
-  });
-
-console.log("this will log first");
+function addNewDoggo() {
+    const promise = fetch(DOG_URL);
+    promise //function runs whenever promis is resolved - needs time to back from server (this is async)
+      .then(function(response) { // it comes as a block of text to resolve it
+        const processingPromise = response.json(); // parsing to object i can use (giving another promis)
+        return processingPromise; // returning another promise (promise chaining)
+      })
+      .then(function(processedResponse) {
+        const img = document.createElement("img");
+        img.src = processedResponse.message; //processing api
+        img.alt = "Cute doggo";
+        doggos.appendChild(img);
+      });
+  }
+  
+  document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
