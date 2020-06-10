5bb5e0981e98ec53d8567c8194df47e18c9e18d7
Kacper <kacwal@poczta.fm>
2020-06-10 12:58:39 +0200

    npm project by package

---
+++ frontend_masters/begginer/integrating_libraries/index.js
@@ -0,0 +1,25 @@
+const popmotion = require('popmotion');
+
+const { styler, spring, listen, pointer, value } = popmotion;
+
+const ball = document.querySelector('.box');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  }); 
