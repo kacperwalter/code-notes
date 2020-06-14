8278f10218e0b1cf038be99c6b055b340da36905
Kacper <kacwal@poczta.fm>
2020-06-14 16:03:33 +0200

    new loop

--- frontend_masters/begginer/getting started with js/index.js
+++ frontend_masters/begginer/getting started with js/index.js
@@ -1,6 +1,21 @@
-const age = 221;
+const people = ['Marek', 'Kacper', 'Zosia', 'Patryk'];
 
-const ageLength = age.length;
-// console.log(ageLength)
+const isEnrolled = (person) => {
+  if (people.includes(person)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-const lame = age;
+const iterator1 = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+const iterator2 = (arr) => {
+  for (let person of arr) {
+    console.log(person);
+  }
+}
