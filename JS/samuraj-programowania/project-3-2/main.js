4a9d3cb5f88fdbf00e520549712212927641f93f
Kacper Walter <kacwal@poczta.fm>
2020-08-10 13:29:02 +0200

    add button and ul

--- JS/samuraj-programowania/project-3-2/main.js
+++ JS/samuraj-programowania/project-3-2/main.js
@@ -2,7 +2,12 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
- // tutaj kod
+    const btn = document.createElement('button');
+    btn.textContent = "Dodaj 10 elementów do listy";
+    document.body.appendChild(btn);
+
+    const ul = document.createElement('ul');
+    document.body.appendChild(ul);
 }
 
 const createLiElements = () => {
