9d617ff549598076dd9edf92fd9716b48b1f20a2
Kacper Walter <kacwal@poczta.fm>
2020-08-10 12:45:37 +0200

    task done with foreach

--- JS/samuraj-programowania/project-3-1/main.js
+++ JS/samuraj-programowania/project-3-1/main.js
@@ -5,10 +5,22 @@ const btn = document.querySelector('button');
 
 let size = 10;
 
+// COMMENT ONE BEFORE USE
+
+// using for loop
 btn.addEventListener('click', function(){
     for(let i = 0; i < liElementsArray.length; i++) {
         liElementsArray[i].style.display = 'block';
         liElementsArray[i].style.fontSize = size + 'px';
     }
     size += 1;
+});
+
+// using forEach - proper way
+btn.addEventListener('click', function() {
+    liElementsArray.forEach(function(element) {
+        element.style.display = 'block';
+        element.style.fontSize = size + 'px';
+    });
+    size++;
 });
