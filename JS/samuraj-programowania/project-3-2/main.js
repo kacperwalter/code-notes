91b9ebb7cf2bd5e301933b2dc6ab57508d2de214
Kacper Walter <kacwal@poczta.fm>
2020-08-10 17:38:09 +0200

    taks done

--- JS/samuraj-programowania/project-3-2/main.js
+++ JS/samuraj-programowania/project-3-2/main.js
@@ -4,14 +4,23 @@ let orderElement = 1;
 const init = () => {
     const btn = document.createElement('button');
     btn.textContent = "Dodaj 10 elementów do listy";
-    document.body.appendChild(btn);
-
     const ul = document.createElement('ul');
+    document.body.appendChild(btn);
     document.body.appendChild(ul);
+
+    btn.addEventListener('click', createLiElements);
 }
 
 const createLiElements = () => {
- // tutaj kod
+    for (let i = 0; i < 10; i++) {
+        const liElement = document.createElement('li');
+        liElement.style.order = orderElement;
+        liElement.style.fontSize = size +'px';
+        liElement.textContent = orderElement;
+        document.querySelector('ul').appendChild(liElement);
+        orderElement++;
+        size++;
+    }
 }
 
 init()
