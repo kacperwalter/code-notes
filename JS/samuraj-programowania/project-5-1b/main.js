fb0ac856418368973695090951e43983629f6c1b
Kacper Walter <kacwal@poczta.fm>
2020-08-11 17:14:23 +0200

    forEach to master

---
+++ JS/samuraj-programowania/project-5-1b/main.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('pass');
+
+const div = document.querySelector('.message');
+
+const passwords = ['user', 'wiosna', 'ania'];
+const messages = ['wiadomość', 'jeszcze jedna wiadomość', 'ania do prania'];
+
+input.addEventListener('input', (e) => {
+    div.textContent = '';
+    const text = e.target.value;
+
+    // foreach to master
+    passwords.forEach((password, index) => {
+        if(password === text) {
+            div.textContent = messages[index];
+        }
+    });
+});
+
+input.addEventListener('focus', (e) => {
+    e.target.classList.toggle('active');
+});
+
+input.addEventListener('blur', (e) => {
+    e.target.classList.toggle('active');
+});
