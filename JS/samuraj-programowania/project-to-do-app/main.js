a2596c8231840487d8c6611c8906d83afca0371d
Kacper Walter <kacwal@poczta.fm>
2020-09-06 19:29:29 +0200

    fast todo app

---
+++ JS/samuraj-programowania/project-to-do-app/main.js
@@ -0,0 +1,19 @@
+const taskAddField = document.getElementById('todo-input');
+const taskAddButton = document.querySelector('.box-button');
+const todoUlList = document.getElementById('todos-ul');
+let todoContent = '';
+
+taskAddField.addEventListener('input', (e) => {
+    todoContent = e.target.value;
+})
+
+taskAddButton.addEventListener('click', (e) => {
+    if (todoContent != '') {
+        const todoElement = document.createElement('li');
+        todoElement.textContent = todoContent;
+        todoUlList.appendChild(todoElement);
+        todoContent = '';
+    }  else {
+        alert('Put value in input area');
+    }
+})
