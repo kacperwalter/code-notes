81c9bb9e79814db5d56bdb33156d9bcc9b5b5cdc
Kacper Walter <kacwal@poczta.fm>
2020-09-16 20:49:53 +0200

    project 6-3 done

--- JS/samuraj-programowania/project-6-3/main.js
+++ JS/samuraj-programowania/project-6-3/main.js
@@ -5,11 +5,17 @@ const addName = (e) => {
     // preventDefault stops default action
     e.preventDefault();
     const input = document.querySelector('input');
-    // adds input when button is clicked
-    const name = input.value;
-    names.push(name);
-    div.textContent += name + ', ';
-    input.value = '';
+    const newName = input.value;
+    if (input.value.length) {
+        for (name of names) {
+            if (name === newName) {
+                return;
+            }
+        }
+        names.push(newName);
+        div.textContent += newName + ', ';
+        input.value = '';
+    }
 }
 
 document.querySelector('button').addEventListener('click', addName);
