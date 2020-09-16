fc7cc143dda6ca01bd4b3fbd62a32deaadb0aaf2
Kacper Walter <kacwal@poczta.fm>
2020-09-16 22:18:36 +0200

    project done

--- JS/samuraj-programowania/project-6-4/main.js
+++ JS/samuraj-programowania/project-6-4/main.js
@@ -4,20 +4,39 @@ const resetButton = document.querySelector('.clean');
 const showAdviceBtn = document.querySelector('.showAdvice');
 const showOptionsBtn = document.querySelector('.showOptions');
 
+let advices = [];
+
 const addPossibility = (e) => {
     e.preventDefault();
+    const input = document.querySelector('input');
+    if (input.value.length != 0) {  
+        const newPossibility = input.value;
+        advices.push(newPossibility);
+        input.value = '';
+    } else {
+        alert('Put value in input area');
+    }
 }
 
 const reset = (e) => {
     e.preventDefault();
+    advices = [];
 }
 
 const showAdvice = (e) => {
     e.preventDefault();
+    const showText = document.querySelector('h1');
+    const randomNumber = Math.floor(Math.random() * advices.length);
+    showText.textContent = advices[randomNumber];
 }
 
 const showOptions = (e) => {
     e.preventDefault();
+    let alertContent = '';
+    for ( advice of advices ) {
+        alertContent += advice + ', '
+    }
+    alert(`Możliwości to: ${alertContent}`);
 }
 
 addButton.addEventListener('click', addPossibility);
