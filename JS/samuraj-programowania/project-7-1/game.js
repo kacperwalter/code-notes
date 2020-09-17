079edea3ee566171a4464513527caa0780e70eb9
Kacper Walter <kacwal@poczta.fm>
2020-09-17 22:00:49 +0200

    switch done

--- JS/samuraj-programowania/project-7-1/game.js
+++ JS/samuraj-programowania/project-7-1/game.js
@@ -8,7 +8,10 @@ let winGames = 0;
 let lostGames = 0;
 let drawGames = 0;
 
-const winner = ["Gracz", "Komputer"];
+const whoIsWinner = ["Gracz", "Komputer", "Remis"];
+let winner;
+
+const whoWinTextArea = document.querySelector('.winner')
 
 const weaponChoice = ['paper', 'rock', 'scissors'];
 
@@ -29,6 +32,58 @@ document.querySelector('.scissors').addEventListener('click', (e) => {
     playerChoice = weaponChoice[2];
     playerChecked = true;
 });
+
+// const weaponChoice = ['paper', 'rock', 'scissors'];
+const battle = (playerWeapon, computerWeapon) => {
+    switch (playerWeapon) {
+        case weaponChoice[0]:
+                if (computerWeapon[1]) {
+                    numberOfGamesPlayed += 1;
+                    winGames += 1;
+                    whoWinTextArea.textContet = whoIsWinner[0];
+                } else if (computerWeapon[2]) {
+                    numberOfGamesPlayed += 1;
+                    lostGames += 1;
+                    whoWinTextArea.textContet = whoIsWinner[1];
+                } else {
+                    numberOfGamesPlayed += 1;
+                    drawGames += 1;
+                    whoWinTextArea.textContet = whoIsWinner[2];
+                }
+            break;
+        case weaponChoice[1]:
+                if (computerWeapon[0]) {
+                    numberOfGamesPlayed += 1;
+                    lostGames += 1;
+                    whoWinTextArea.textContet = whoIsWinner[1];
+                } else if (computerWeapon[2]) {
+                    numberOfGamesPlayed += 1;
+                    winGames += 1;
+                    whoWinTextArea.textContent = whoIsWinner[0];
+                } else {
+                    numberOfGamesPlayed += 1;
+                    drawGames += 1;
+                    whoWinTextArea.textContent = whoIsWinner[2];
+                }
+            break;
+        case weaponChoice[2]:
+                if (computerWeapon[0]) {
+                    numberOfGamesPlayed += 1;
+                    winGames += 1;
+                    whoWinTextArea.textContent = whoIsWinner[0];
+                } else if (computerWeapon[1]) {
+                    numberOfGamesPlayed += 1;
+                    lostGames += 1;
+                    whoWinTextArea.textContent = whoIsWinner[1];
+                } else {
+                    numberOfGamesPlayed += 1;
+                    drawGames += 1;
+                    whoWinTextArea.textContent = whoIsWinner[2];
+                }
+            break;
+
+    }
+}
 
 document.querySelector('.start').addEventListener('click', (e) => {
     if (playerChecked) {
