2dd070f0898cfc23cf920bdc1ff9de326ee71c77
Kacper Walter <kacwal@poczta.fm>
2020-09-20 16:56:18 +0200

    new project

---
+++ JS/samuraj-programowania/project-7-2/game.js
@@ -0,0 +1,10 @@
+// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+// obiektówa - kodowane razem z prowadzącym po zakodowaniu najpierw samemu
+
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0
+}
+gft
