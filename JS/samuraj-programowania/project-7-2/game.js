a3d080ae7fd5ea2301ee2549ee8037553e8ae637
Kacper Walter <kacwal@poczta.fm>
2020-09-22 21:24:27 +0200

    game functions

--- JS/samuraj-programowania/project-7-2/game.js
+++ JS/samuraj-programowania/project-7-2/game.js
@@ -7,4 +7,35 @@ const gameSummary = {
     losses: 0,
     draws: 0
 }
-gft
+
+const game = {
+    playerHand: null,
+    aiHand: null
+}
+
+// rest operator ([...smth]) makes array from for ex. NodeList (array has more methods than NodeList)
+const hands = [...document.querySelectorAll('.select img')];
+
+// there we use this but in arrow function we pass e and operating on e (event)
+function handSelecion() {
+    // dataset because we use data-xyz in html
+    game.playerHand = this.dataset.option;
+    hands.forEach(item => item.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 4px red';
+}
+
+function aiChoice() {
+    return hands[Math.floor(Math.random() * 3)].dataset.option;
+}
+
+function startGame() {
+    // without return function will run continiously
+    if(!game.playerHand) return alert('Choose weapon!');
+    game.aiHand = aiChoice();
+}
+
+hands.forEach((item, index) => {
+    item.addEventListener('click', handSelecion);
+})
+
+document.querySelector('.start').addEventListener('click', startGame);
