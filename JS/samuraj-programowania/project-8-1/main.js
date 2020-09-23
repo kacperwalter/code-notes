13a687844297f5552a484eb40c898f3168d1dc86
Kacper Walter <kacwal@poczta.fm>
2020-09-23 19:06:09 +0200

    setInterval exercise done

--- JS/samuraj-programowania/project-8-1/main.js
+++ JS/samuraj-programowania/project-8-1/main.js
@@ -1,4 +1,6 @@
 const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+
 const txt = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? KONIEC'
 
 let indexText = 0;
@@ -10,4 +12,9 @@ const addLetter = () => {
     if (indexText == txt.length) clearInterval(indexTyping);
 }
 
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+}
+
 const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
