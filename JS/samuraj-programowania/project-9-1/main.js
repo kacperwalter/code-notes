0f611d3de3c0f374e5753210ae5e739678805d07
Kacper Walter <kacwal@poczta.fm>
2020-09-26 19:55:22 +0200

    slider done

--- JS/samuraj-programowania/project-9-1/main.js
+++ JS/samuraj-programowania/project-9-1/main.js
@@ -14,19 +14,33 @@ const slideList = [
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')];
 
 // Interfejs
 const time = 5000;
 let active = 0;
 
-const changeSlide = () => {
-    active++;
-    image.src = slideList[active].img;
-    h1.textContent = slideList[active].text;
-}
-
-// setInterval(changeSlide, time);
 
 // Implementacja
+
+const changeDot = () => {
+    // findIndex returns index of element in array when it has something what we want to find
+    const adctiveDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[adctiveDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+const changeSlide = () => {
+    active++;
+    if (active === slideList.length) {
+        active = 0;
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot();
+}
+
+setInterval(changeSlide, time);
+
 
 
