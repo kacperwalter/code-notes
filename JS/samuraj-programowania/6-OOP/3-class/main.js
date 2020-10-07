45afc3bd0fabbdcc5f3d848e6ca51eb3550e8d63
Kacper Walter <kacwal@poczta.fm>
2020-10-07 20:17:17 +0200

    inheritance

--- JS/samuraj-programowania/6-OOP/3-class/main.js
+++ JS/samuraj-programowania/6-OOP/3-class/main.js
@@ -7,7 +7,7 @@ class FamilyTest {
     }
   }
   
-  const nowakowie = new FamilyTest();
+const nowakowie = new FamilyTest();
   
   class Family {
     constructor(members, ...names) {
@@ -21,7 +21,7 @@ class FamilyTest {
     static makeFamily(...members) {
       return members;
     }
-  }
+}
   
   const kowalscy = new Family(3, "Jan", "Ewa", "Adam");
   
@@ -36,4 +36,52 @@ class FamilyTest {
 
 
 // INHERITANCE - extends and super 
+// Klasy nadrzędna (superclass) - ta, z której jest dziediczone
+// Klasy podrzędne (subclass) - te, które dziedziczą
 
+// Klasa podrzędna tworzy nowy prototyp w opraciu o klasę nadrzędną - powwstaje łańcuch prototypów (klasa dziedzicząca ma dostęp do prototypów klasy wyższej)
+
+// extends - wskazanie klasy po której nowa klasa powinna dziedziczyć
+// super - rozszerzania konstruktora nowej klasy o konstruktor klasy z której dziedziczy
+
+class Animal {
+    constructor(age, name) {
+        this.age = age;
+        this.name = name;
+    }
+    breathe() {
+        console.log("Zwierze oddycha");
+    }  
+}
+
+const zwierze = new Animal(12);
+
+console.log(zwierze.__proto__ == Animal.prototype);
+
+class Mammal extends Animal {
+    constructor(age, hair, name) {
+        super(age, name);
+        // metoda super wywołuje poniższy kod (dziedziczy prototypy klasy nadrzędniej)
+        // constructor(age) {
+        //     this.age = age;
+        // }
+        this.hair = hair;
+    }
+    drinkMilk() {
+        console.log("ssaki piją mleko");
+    }
+}
+
+const ssak = new Mammal(3, 'blond', 'andrzej'); // łańcuch prototypów - prototypem nadrzędnym jest klasa Animal, a w klasie Animal jest prototyp breathe()
+
+class Human extends Mammal {
+    constructor(age, name, hair, language) {
+        super(age, name, hair);
+        this.language = language;
+    }
+    speak() {
+        console.log('Człowiek mówi');
+    }
+}
+
+const czlowiek = new Human(34, "Jarek", 'brazowe', 'polszowy');
