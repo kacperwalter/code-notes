6f72b3a8dccf12ba0d356f044b54f3e08b24a304
Kacper Walter <kacwal@poczta.fm>
2020-11-25 23:41:29 +0100

    Fetch done

--- JS/zaawansowany-JS-CSS/7-AJAX/main.js
+++ JS/zaawansowany-JS-CSS/7-AJAX/main.js
@@ -16,6 +16,7 @@ const testFetch = (e) => {
     .catch((err) => console.log(err))
 }
 
+
 const getUsers = (e) => {
   e.preventDefault();
 
@@ -23,7 +24,30 @@ const getUsers = (e) => {
   const usersGender = document.querySelector('[name="gender"]').value;
   
   const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${usersGender === "both" ? "male,female" : usersGender}`;
-  console.log(url);
+
+  fetch(url)
+    .then(response => {
+    if (response.status !== 200) {
+      throw Error("Not 200");
+    } else {
+      return response.json(); // tutaj tworzy się obiekt do którego właściwości możemy się odwłowywać
+    }
+  })
+  .then(json => showUsers(json.results))
+  .catch(err => console.log(err))
+}
+
+const showUsers = (users) => {
+  const resultArea = document.querySelector('.user-list');
+  users.forEach((user) => {
+    const item = document.createElement('div');
+    item.className = "user";
+    item.innerHTML = `
+    <div class="user__name">${user.name.title.toUpperCase()} ${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}</div>
+    <img class="user__image" src="${user.picture.medium}">
+    `
+    resultArea.appendChild(item);
+  });
 }
  
 document.querySelector(".generator").addEventListener('submit', getUsers);
