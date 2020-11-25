329759302ee1241c9abed7c14e24525f2be7d74e
Kacper Walter <kacwal@poczta.fm>
2020-11-25 22:04:26 +0100

    simple fetch

--- JS/zaawansowany-JS-CSS/7-AJAX/main.js
+++ JS/zaawansowany-JS-CSS/7-AJAX/main.js
@@ -1,7 +1,17 @@
 const getUsers = () => {
-  const url = 'https://randomuser.me/api/?results=10'
-  fetch(url)
-    .then((reponse) => console.log(reponse))
+  const url = 'https://randomuser.me/api/?results=10';
+  fetch(url) // obietnica - oczekujący (pending)
+  // kiedy obietnica zostanie rozstrzygnięta (spełnione lub odrzucone)
+  // pozytywnie - wykonuje się then, negatywnie - wykonuje się catch
+    .then((response) => { // kolejna obietnica (promise)
+      // console.log(response)
+      if (response.status !== 200) {
+        throw Error("to nie jest odpowiedź 200");
+      } else {
+        return response.json();
+      }
+    })
+    .then(json => console.log(json))
     .catch((err) => console.log(err))
 }
  
