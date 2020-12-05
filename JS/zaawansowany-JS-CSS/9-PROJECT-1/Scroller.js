f4eec043031f8deee952bbd60102c7278e06d88c
Kacper Walter <kacwal@poczta.fm>
2020-12-05 17:50:55 +0100

    Fixed ; bug

--- JS/zaawansowany-JS-CSS/9-PROJECT-1/Scroller.js
+++ JS/zaawansowany-JS-CSS/9-PROJECT-1/Scroller.js
@@ -8,7 +8,7 @@ class Scroller {
     }
 
     isScrolledIntoView(el) {
-        const rect = el.getBoundingClientRect()
+        const rect = el.getBoundingClientRect();
         const elemTop = rect.top;
         const elemBottom = rect.bottom;
         const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
