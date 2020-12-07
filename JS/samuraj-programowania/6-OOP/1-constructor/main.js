cee8edea7ee9ef96775e1e4fcab726c1bb7bce48
Kacper Walter <kacwal@poczta.fm>
2020-12-07 21:59:01 +0100

    Basic OOP in JS

--- JS/samuraj-programowania/6-OOP/1-constructor/main.js
+++ JS/samuraj-programowania/6-OOP/1-constructor/main.js
@@ -10,3 +10,31 @@ const polonez = new Car('polonez');
 // 4. wywołanie funkcji i przypisanie do zmiennej
 
 maluch.name = "zabytek";
+
+// przykładowy obiekt
+const Human = function(name, legs, hands, eyeColor) {
+    this.name = name;
+    this.legs = legs;
+    this.hands = hands;
+    this.eyeColor = eyeColor;
+
+    this.printName = function() {
+        console.log(`Ma na imię ${this.name}`);
+    }
+
+    this.howManyLegs = function() {
+        if (this.legs == 1) {
+            console.log(`Ma ${this.legs} nogę`);
+        } else {
+            console.log(`Ma ${this.legs} nóg`);
+        }
+    }
+
+    this.screamXTimes = function(times) {
+        for (let i=0; i<=times; i++) {
+            console.log(`${i} ${this.name}`);
+        }
+    }
+}
+
+const zosiaczul = new Human('Zosia', 2, 2, 'brązowy');
