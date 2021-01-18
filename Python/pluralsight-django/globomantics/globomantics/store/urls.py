c1720f5389662dceacc3554dbe37275a22bd3b81
Kacper Walter <kacwal@poczta.fm>
2021-01-18 20:17:59 +0100

    advanced URLs

--- Python/pluralsight-django/globomantics/globomantics/store/urls.py
+++ Python/pluralsight-django/globomantics/globomantics/store/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # using regexes to build urls
+    re_path(r'^\d+', views.detail, name='detail'),
+    re_path(r'^electronics', views.electronics, name='electronics')
 ]
--- Python/pluralsight-django/globomantics/globomantics/store/views.py
+++ Python/pluralsight-django/globomantics/globomantics/store/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Test str with livesync")
+    return HttpResponse("Welcome to store!")
+
+
+def detail(request):
+    return HttpResponse("Hello there, this is detailed website")
+
+
+def electronics(request):
+    return HttpResponse("Hello there, this is subpage for electronics")
