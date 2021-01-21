4948f38cb9878a6558171c7e439c89a0e350c8ec
Kacper Walter <kacwal@poczta.fm>
2021-01-21 18:49:26 +0100

    Paginators

--- Python/pluralsight-django/globomantics/globomantics/store/views.py
+++ Python/pluralsight-django/globomantics/globomantics/store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.cache import cache_page
+
+from django.core.paginator import Paginator
 
 def index(request):
     return HttpResponse("Welcome to store!")
@@ -10,5 +15,19 @@ def detail(request):
     return HttpResponse("Hello there, this is detailed website")
 
 
+# decorators
+@csrf_exempt
+@cache_page(900)
+@require_http_methods(["GET"])
 def electronics(request):
-    return HttpResponse("Hello there, this is subpage for electronics")
+    items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
+    if request.method == 'GET':
+        paginator = Paginator(items, 2)
+        pages = request.GET.get('page', 1)
+        try:
+            items = paginator.page(pages)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        return render(request, 'store/list.html', {'items': items})
+    elif request.method == 'POST':
+        return HttpResponseNotFound("POST is not allowed")
