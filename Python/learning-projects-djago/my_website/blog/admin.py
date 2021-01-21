596c009eca450f4bff02cdac4bbc79ed62d1c021
Kacper Walter <kacwal@poczta.fm>
2021-01-21 20:24:40 +0100

    Added pagination to project

--- Python/learning-projects-djago/my_website/blog/admin.py
+++ Python/learning-projects-djago/my_website/blog/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Category, Entry, Author
 
 
-admin.site.register(Category)
-admin.site.register(Entry)
-admin.site.register(Author)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
--- Python/learning-projects-djago/my_website/blog/views.py
+++ Python/learning-projects-djago/my_website/blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 from .models import Entry
 
 
 def entry_list(request):
+    entries = Entry.objects.all()
+    paginator = Paginator(entries, 3)
+    pages = request.GET.get('page', 1)
+    try: 
+        entries = paginator.page(pages)
+    except PageIsNotAnInteger:
+        entries = paginator.page(1)
     context = {
-        "entries": Entry.objects.all(),
+        "entries": entries,
     }
     return render(request, 'blog/entry_list.html', context)
 
