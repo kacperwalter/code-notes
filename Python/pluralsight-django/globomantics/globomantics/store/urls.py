c92feda457990fa10ca05166067f80b0bec65bdc
Kacper Walter <kacwal@poczta.fm>
2021-01-25 18:23:41 +0100

    pagination

--- Python/pluralsight-django/globomantics/globomantics/store/urls.py
+++ Python/pluralsight-django/globomantics/globomantics/store/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, re_path
 
 from . import views
+from .views import ElectronicsView
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     # using regexes to build urls
     re_path(r'^\d+', views.detail, name='detail'),
-    re_path(r'^electronics', views.electronics, name='electronics')
+    # re_path(r'^electronics', views.electronics, name='electronics'),
+    re_path(r'^electronics', ElectronicsView.as_view(), name='electronics'),
+    re_path(r'^pagination', views.additional_pagination, name="pagination"),
 ]
--- Python/pluralsight-django/globomantics/globomantics/store/views.py
+++ Python/pluralsight-django/globomantics/globomantics/store/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.cache import cache_page
 
 from django.core.paginator import Paginator
+
 
 def index(request):
     return HttpResponse("Welcome to store!")
@@ -15,13 +17,29 @@ def detail(request):
     return HttpResponse("Hello there, this is detailed website")
 
 
-# decorators
-@csrf_exempt
-@cache_page(900)
-@require_http_methods(["GET"])
-def electronics(request):
-    items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
-    if request.method == 'GET':
+# FUNCTION BASED VIEW
+# decorators and paginators
+# @csrf_exempt
+# @cache_page(900)
+# @require_http_methods(["GET"])
+# def electronics(request):
+#     items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
+#     if request.method == 'GET':
+#         paginator = Paginator(items, 2)
+#         pages = request.GET.get('page', 1)
+#         try:
+#             items = paginator.page(pages)
+#         except PageNotAnInteger:
+#             items = paginator.page(1)
+#         return render(request, 'store/list.html', {'items': items})
+#     elif request.method == 'POST':
+#         return HttpResponseNotFound("POST is not allowed")
+
+
+# CLASS BASED VIEW
+class ElectronicsView(View):
+    def get(self, request):
+        items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
         paginator = Paginator(items, 2)
         pages = request.GET.get('page', 1)
         try:
@@ -29,5 +47,8 @@ def electronics(request):
         except PageNotAnInteger:
             items = paginator.page(1)
         return render(request, 'store/list.html', {'items': items})
-    elif request.method == 'POST':
-        return HttpResponseNotFound("POST is not allowed")
+
+
+def additional_pagination(request):
+    items = ("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8")
+    return render(request, 'store/pagination.html', {})
