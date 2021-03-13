0a3c11923c66d21b833ab98cefbcee128adae87c
Kacper Walter <kacwal@poczta.fm>
2021-03-13 17:18:35 +0100

    Webpack configured

--- JS/samuraj-programowania/nodejs/webpack4/src/tools/image.js
+++ JS/samuraj-programowania/nodejs/webpack4/src/tools/image.js
@@ -1,7 +1,9 @@
 import photo from '../images/zdjecie.png';
+import $ from 'jquery';
 
 export default (tag) => {
-    const img = document.createElement('img');
-    img.src = photo;
-    document.querySelector(tag).appendChild(img);
+    // const img = document.createElement('img');
+    // img.src = photo;
+    // document.querySelector(tag).appendChild(img);
+    $(tag).append(`<img src=${photo}>`);
 }
