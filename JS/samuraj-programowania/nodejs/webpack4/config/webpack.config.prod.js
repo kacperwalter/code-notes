e31a4b99017ded2288523e8741a782935a6535f4
Kacper Walter <kacwal@poczta.fm>
2021-03-13 14:11:34 +0100

    Production and development configs

--- JS/samuraj-programowania/nodejs/webpack4/config/webpack.config.prod.js
+++ JS/samuraj-programowania/nodejs/webpack4/config/webpack.config.prod.js
@@ -1,15 +1,16 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
 
 const webpackConfig = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         main: './src/app.js',
     },
     output: {
-        filename: 'js/[name].js',
+        filename: 'js/[contenthash:6].main.js',
         path: path.resolve(__dirname, '../', 'build'),
     },
     module: {
@@ -20,7 +21,7 @@ const webpackConfig = {
             },
             {
                 test: /\.css$/,
-               use: ['style-loader', 'css-loader']
+               use: [MiniCssExtractPlugin.loader, 'css-loader'], // uzywajac MiniCssExtractPlugin nie potrzebujemy style-loadera
            },
            {
                  test: /\.txt$/,
@@ -29,7 +30,7 @@ const webpackConfig = {
             {
                 // do tego musi być zainstalowany dodatkowo node-sass
                 test: /\.(sass|scss)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             }
         ],
     },
@@ -38,12 +39,10 @@ const webpackConfig = {
         new HtmlWebpackPlugin({
             template: "src/templates/template.html"
         }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name]-[contenthash].css',
+        }),
     ],
-    devServer: {
-        open: true,
-        contentBase: path.resolve(__dirname, '..', 'public'),
-        port: 8080,
-    }
 };
 
 module.exports = webpackConfig;
