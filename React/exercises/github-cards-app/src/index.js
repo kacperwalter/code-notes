b7f2a0c882bfe8fd85fdeb2d3e4cd8e2248d8ef2
Kacper Walter <kacwal@poczta.fm>
2021-03-20 16:07:42 +0100

    exercise

--- React/exercises/github-cards-app/src/index.js
+++ React/exercises/github-cards-app/src/index.js
@@ -1,3 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+/*
+	const testData = [
+			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+	];
+*/
+
+// Card
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="https://placehold.it/75" />
+                <div className="info">
+                    <div className="name">Name here...</div>
+                    <div className="company">Company here...</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// List
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Card />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App title="The Github Cards App" />,
+    document.getElementById('root'),
+)
