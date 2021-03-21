63aa1f1727503bf08bd075dac163d5454fc2651b
Kacper Walter <kacwal@poczta.fm>
2021-03-21 17:16:46 +0100

    Some react building

--- React/exercises/github-cards-app/src/index.js
+++ React/exercises/github-cards-app/src/index.js
@@ -2,41 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/*
-	const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-	];
-*/
 
-// Card
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  	{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
+class FetchApi {
+    constructor(url) {
+        this.url = url;
+    }
+    async fetch() {
+        const resp = await fetch(this.url);
+        const data = await resp.json();
+        return data; 
+    }
+}
+
+const fetchProfile = new FetchApi('https://api.github.com/users/kacperwalter');
+// fetchProfile.fetch().then(data => console.log(data.login));
+
+
+const CardList = (props) => {
+    return(
+    <div>
+        {testData.map(profile => <Card {...profile}/>)}
+    </div>
+    )
+};
+
 class Card extends React.Component {
     render() {
+        const profile = this.props;
         return (
             <div className="github-profile">
-                <img src="https://placehold.it/75" />
+                <img src={profile.avatar_url}/>
                 <div className="info">
-                    <div className="name">Name here...</div>
-                    <div className="company">Company here...</div>
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
                 </div>
             </div>
         );
     }
 }
 
-// List
+class Form extends React.Component {
+    render() {
+        return (
+            <form action="">
+                <input type="text" placeholder="GitHub username"/>
+                <button>AddCard</button>
+            </form>
+        );
+    }
+}
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
-                <Card />
+                <CardList />
             </div>
         );
     }
 }
-
 
 ReactDOM.render(
     <App title="The Github Cards App" />,
