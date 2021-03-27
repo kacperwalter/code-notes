3f5df6ceab3a7df65fdb348aab99109642a17bbf
Kacper Walter <kacwal@poczta.fm>
2021-03-27 16:04:03 +0100

    m

--- React/helloroman-react/study-buddy/src/index.js
+++ React/helloroman-react/study-buddy/src/index.js
@@ -1,3 +1,4 @@
+// dzięki importom absolutnym możemy robić importy bez wskazywania konkretnego folderu w którym znajdują się importowane elementy - możemy to uzyskać dzięki jsconfig.json (uwzględniony w tym projekcie) - więcej w dokumentacji create-react-app (absolute imports)
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
--- React/helloroman-react/study-buddy/src/views/App.js
+++ React/helloroman-react/study-buddy/src/views/App.js
@@ -1,5 +1,7 @@
-import logo from '../assets/logo.svg';
+import logo from 'assets/logo.svg';
 import './App.css';
+
+console.log('true');
 
 function App() {
   return (
