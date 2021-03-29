3016292faff47396c172f60004bb12adfac3cdd5
Kacper Walter <kacwal@poczta.fm>
2021-03-29 21:08:14 +0200

    Global styles

--- React/helloroman-react/study-buddy/src/views/Root.js
+++ React/helloroman-react/study-buddy/src/views/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UsersList from 'components/organisms/UsersList/UsersList';
 import styled from 'styled-components';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
 
 const Wrapper = styled.div`
   background-color: #f7f8fa;
@@ -12,9 +13,12 @@ const Wrapper = styled.div`
 `;
 
 const Root = () => (
-  <Wrapper>
-    <UsersList />
-  </Wrapper>
+  <>
+    <GlobalStyle />
+    <Wrapper>
+      <UsersList />
+    </Wrapper>
+  </>
 );
 
 export default Root;
