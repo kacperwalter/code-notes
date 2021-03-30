92138a1af17ab9090de1dc21b649d964318e5817
Kacper <kacwal@poczta.fm>
2021-03-30 11:46:16 +0200

    Found good way to do it

--- React/helloroman-react/study-buddy/src/components/atoms/Average/Average.js
+++ React/helloroman-react/study-buddy/src/components/atoms/Average/Average.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyledAverage } from './Average.styles';
 
-const Average = (props) => <StyledAverage>{props.avgData}</StyledAverage>;
+const Average = (props) => <StyledAverage avgValue={props.avgData}>{props.avgData}</StyledAverage>;
 
 export default Average;
--- React/helloroman-react/study-buddy/src/components/atoms/Average/Average.styles.js
+++ React/helloroman-react/study-buddy/src/components/atoms/Average/Average.styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+const colorSwitcher = (avgValue, theme) => {
+  if (avgValue < 3) {
+    return theme.colors.error;
+  } else if (avgValue > 3 && avgValue < 4.5) {
+    return theme.colors.warning;
+  } else {
+    return theme.colors.success;
+  }
+};
+
 export const StyledAverage = styled.div`
-  color: red;
+  color: ${({ avgValue, theme }) => colorSwitcher(avgValue, theme)};
 `;
