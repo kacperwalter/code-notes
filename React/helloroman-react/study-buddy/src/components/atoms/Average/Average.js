50cf3685be52b2984b084625db4918c77d7867fb
Kacper <kacwal@poczta.fm>
2021-03-30 13:02:38 +0200

    Add styling

--- React/helloroman-react/study-buddy/src/components/atoms/Average/Average.js
+++ React/helloroman-react/study-buddy/src/components/atoms/Average/Average.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StyledAverage } from './Average.styles';
 
-const Average = (props) => <StyledAverage avgValue={props.avgData}>{props.avgData}</StyledAverage>;
+const Average = (props) => (
+  <StyledAverage avgValue={props.avgData}>
+    <p>{props.avgData}</p>
+  </StyledAverage>
+);
 
 export default Average;
--- React/helloroman-react/study-buddy/src/components/atoms/Average/Average.styles.js
+++ React/helloroman-react/study-buddy/src/components/atoms/Average/Average.styles.js
@@ -3,15 +3,20 @@ import styled from 'styled-components';
 // TODO
 // I can use ternary operator here somehow
 const colorSwitcher = (avgValue, theme) => {
-  if (avgValue < 3) {
-    return theme.colors.error;
-  } else if (avgValue > 3 && avgValue < 4.5) {
-    return theme.colors.warning;
-  } else {
-    return theme.colors.success;
-  }
+  if (avgValue < 3) return theme.colors.error;
+  if (avgValue > 3 && avgValue < 4.5) return theme.colors.warning;
+  if (avgValue >= 4.5) return theme.colors.success;
 };
 
 export const StyledAverage = styled.div`
-  color: ${({ avgValue, theme }) => colorSwitcher(avgValue, theme)};
+  width: 34px;
+  height: 34px;
+  border-radius: 50px;
+  background-color: ${({ avgValue, theme }) => colorSwitcher(avgValue, theme)};
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: 700;
+  font-size: ${({ theme }) => theme.fontSize.m};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
