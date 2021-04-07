d4112c4e14f39042300584e6c36de055d0938dc6
Kacper <kacwal@poczta.fm>
2021-04-07 15:32:50 +0200

    Class component

--- React/helloroman-react/study-buddy/src/components/organisms/UsersList/UsersList.js
+++ React/helloroman-react/study-buddy/src/components/organisms/UsersList/UsersList.js
@@ -3,10 +3,39 @@ import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper } from './UsersList.styles';
 
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (users) {
+        resolve([...users]);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, 2000);
+  });
+};
+
 class UsersList extends React.Component {
   state = {
-    users,
+    users: [],
+    isLoading: false,
   };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    mockAPI().then((data) => {
+      this.setState({ isLoading: false });
+      this.setState({ users: data });
+    });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.isLoading !== this.state.isLoading) {
+      console.log('Loading state has changed');
+    }
+  }
+
+  componentWillUnmount() {}
 
   deleteUser = (name) => {
     const filteredUsers = this.state.users.filter((user) => user.name !== name);
@@ -16,6 +45,7 @@ class UsersList extends React.Component {
   render() {
     return (
       <Wrapper>
+        <h1>{this.state.isLoading ? 'Loading...' : ''}</h1>
         <ul>
           {this.state.users.map((userData, i) => (
             <UsersListItem deleteUser={this.deleteUser} index={i} userData={userData} />
