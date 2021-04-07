17d85868857a67474f9bb6cfda0ca392cdf930c7
Kacper <kacwal@poczta.fm>
2021-04-07 12:27:03 +0200

    Made class component

--- React/helloroman-react/study-buddy/src/components/organisms/UsersList/UsersList.js
+++ React/helloroman-react/study-buddy/src/components/organisms/UsersList/UsersList.js
@@ -3,14 +3,34 @@ import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper } from './UsersList.styles';
 
-const UsersList = () => (
-  <Wrapper>
-    <ul>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} userData={userData} />
-      ))}
-    </ul>
-  </Wrapper>
-);
+// const UsersList = () => (
+//   <Wrapper>
+//     <ul>
+//       {users.map((userData, i) => (
+//         <UsersListItem index={i} userData={userData} />
+//       ))}
+//     </ul>
+//   </Wrapper>
+// );
+
+class UsersList extends React.Component {
+  state = {
+    listTitle: `User's List:`,
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1>{this.state.listTitle}</h1>
+        <button onClick={() => this.setState({ listTitle: 'Students list' })}>Change title</button>
+        <ul>
+          {users.map((userData, i) => (
+            <UsersListItem index={i} userData={userData} />
+          ))}
+        </ul>
+      </Wrapper>
+    );
+  }
+}
 
 export default UsersList;
