a02967134248d47f90ebdd6a9d047946a060066b
Kacper Walter <kacperwalter@MacBook-Pro-Kacper-W.local>
2021-06-19 16:48:37 +0200

    Absolute imports

--- React/helloroman-react/study-buddy/src/views/App.js
+++ React/helloroman-react/study-buddy/src/views/App.js
@@ -1,4 +1,4 @@
-import logo from '../assets/logo.svg';
+import logo from 'assets/logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Edit <code>src/App.js</code> and save to reload.</code>
         </p>
         <a
           className="App-link"
