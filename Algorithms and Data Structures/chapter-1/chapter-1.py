b13af5ef2507e83f65fa9df79b219839dc03f545
Kacper Walter <kacperwalter@MacBook-Pro-Kacper-W.local>
2021-06-30 20:58:02 +0200

    Chapter1 done

--- Algorithms and Data Structures/chapter-1/chapter-1.py
+++ Algorithms and Data Structures/chapter-1/chapter-1.py
@@ -1,6 +1,9 @@
+# notacja duzego O (big O notation wyraza zawsze czas w najgorszym wypadku)
+
+
 def binary_search(list, item):
-    low = 0
-    high = len(list) - 1
+    low = 0                             # pierwszy element tablicy
+    high = len(list) - 1                # ostatni element tablicy (dlugosc tablicy - 1 bo indeksy zaczynaja sie od 0)
 
     while low <= high:
         mid = (low + high)
@@ -11,8 +14,8 @@ def binary_search(list, item):
             high = mid - 1
         else:
             low = mid + 1
-            return None
+    return None
 
 my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, 3))
+print(binary_search(my_list, 7))
