c2def6059f34ef8c761aa1045189b7bc126ec945
Kacper Walter <kacper.walter@netguru.com>
2021-11-07 22:44:53 +0100

    callbacks

--- JS/repetition/index.js
+++ JS/repetition/index.js
@@ -4,12 +4,17 @@ const numbers = [1, 2, 3, 4, 5, 6];
 
 // map is used to do the transformations on arrays
 const doubledNumbers = numbers.map(n => n * 2);
-log(doubledNumbers);
+// log(doubledNumbers);
 
 // filter is used to filter values on array
 const filteredNumbers = numbers.filter(n => n % 2 == 0);
-log(filteredNumbers);
+// log(filteredNumbers);
 
 // reduce
 const reducedNumbers = numbers.reduce((accumulator, current) => accumulator + current);
-log(reducedNumbers);
+// log(reducedNumbers);
+
+// callbacks
+// callback is a function that is passed into another function and then that function in executed at some point
+const callbackExample = (name, callback) => log(callback(name));
+callbackExample("Kacper Walter", (name) => `Hello ${name}`);
