54ba4bb3990bf09d97b907dabbad1eca1fd2714a
Kacper Walter <kacper.walter@netguru.com>
2021-11-17 23:26:37 +0100

    build with root component

--- React/study-buddy/study-buddy/src/components/UsersList/UsersList.js
+++ React/study-buddy/study-buddy/src/components/UsersList/UsersList.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { users } from 'data/users';
-import PropTypes from 'prop-types';
 
 const UsersList = () => (
   <div>
     <ul>
-      {users.map(({name, average, attendance}) => (
+      {users.map(({ name, average, attendance }) => (
         <li>
-        <div>{average}</div>
-        <div>
-          <p>{name}</p>
-          <p>{attendance}</p>
-        </div>
-        <button>X</button>
-      </li>
+          <div>{average}</div>
+          <div>
+            <p>{name}</p>
+            <p>{attendance}</p>
+          </div>
+          <button>X</button>
+        </li>
       ))}
     </ul>
   </div>
 );
-
-UsersList.propTypes = {};
 
 export default UsersList;
