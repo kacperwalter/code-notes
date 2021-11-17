af76eead5340a38f4029d2a9e1f29c47ce711a2d
Kacper Walter <kacper.walter@netguru.com>
2021-11-17 23:17:56 +0100

    Add UsersList component

---
+++ React/study-buddy/study-buddy/src/components/UsersList/UsersList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { users } from 'data/users';
+import PropTypes from 'prop-types';
+
+const UsersList = () => (
+  <div>
+    <ul>
+      {users.map(({name, average, attendance}) => (
+        <li>
+        <div>{average}</div>
+        <div>
+          <p>{name}</p>
+          <p>{attendance}</p>
+        </div>
+        <button>X</button>
+      </li>
+      ))}
+    </ul>
+  </div>
+);
+
+UsersList.propTypes = {};
+
+export default UsersList;
