e01c9a8bac1a19b1e8edafe02da07bcd6edd6d93
Kacper Walter <kacper.walter@netguru.com>
2021-11-18 20:56:23 +0100

    add PropTypes

--- React/study-buddy/study-buddy/src/components/UsersListItem/UsersListItem.js
+++ React/study-buddy/study-buddy/src/components/UsersListItem/UsersListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const UsersListItem = ({ average, name, attendance }) => (
+const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
   <li>
     <div>{average}</div>
     <div>
@@ -10,5 +11,13 @@ const UsersListItem = ({ average, name, attendance }) => (
     <button>X</button>
   </li>
 );
+
+UsersListItem.propTypes = {
+  userData: PropTypes.shape({
+    avarage: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    attendance: PropTypes.string,
+  }),
+};
 
 export default UsersListItem;
