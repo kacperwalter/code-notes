9d1d483409593a90f714107dc303a544912d0848
Kacper Walter <52107523+kacperwalter@users.noreply.github.com>
2022-03-28 21:26:06 +0200

    Users list styling (#59)
    * install node-sass
    
    * scss wip
    
    * installed styled components
    
    * some styling and remove import react
    
    * wip
    
    * Passing props into styled component
    
    * prettier printWidth edit
    
    * .eslintrc error => warning + prettier config inside eslint
    
    * wip
    
    * wip
    
    Co-authored-by: Kacper Walter <kacper.walter@netguru.com>

--- React/study-buddy/study-buddy/src/components/UsersList/UsersList.js
+++ React/study-buddy/study-buddy/src/components/UsersList/UsersList.js
@@ -1,15 +1,25 @@
-import React from 'react';
 import { users } from 'data/users';
+import styled from 'styled-components';
 import UsersListItem from 'components/UsersListItem/UsersListItem';
 
+// to moze nazywac sie wrapper, mimo uzycia tej nazwy w innej czesci projektu, poniewaz kazdy styled-component w innym pliku dostaje finalnie inna nazwe w DOM
+const Wrapper = styled.div`
+  background-color: white;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
+`;
+
 const UsersList = () => (
-  <div>
+  <Wrapper>
     <ul>
       {users.map((userData) => (
         <UsersListItem userData={userData} />
       ))}
     </ul>
-  </div>
+  </Wrapper>
 );
 
 export default UsersList;
--- React/study-buddy/study-buddy/src/components/UsersListItem/UsersListItem.js
+++ React/study-buddy/study-buddy/src/components/UsersListItem/UsersListItem.js
@@ -1,15 +1,40 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2px;
+    background-color: lightgray;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 25px;
+  height: 25px;
+  background-color: ${({ isSecondary }) => (isSecondary ? '#e7e044' : '#c0c7d6')};
+  border-radius: 50px;
+  border: none;
+  color: white;
+`;
 
 const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
-  <li>
+  <Wrapper>
     <div>{average}</div>
     <div>
       <p>{name}</p>
       <p>{attendance}</p>
     </div>
-    <button>X</button>
-  </li>
+    <StyledButton>X</StyledButton>
+    <StyledButton>X</StyledButton>
+  </Wrapper>
 );
 
 UsersListItem.propTypes = {
