744a2690ec0256cff07b4929f2ba2e2a1c7f671c
Kacper Walter <52107523+kacperwalter@users.noreply.github.com>
2022-05-24 09:57:07 +0200

    just start of coding in that course (#87)

---
+++ JS/js-the-weird-parts/app.js
@@ -0,0 +1,10 @@
+// global envoirement and the global object
+
+// the base execution context is the global execution context
+// GLOBAL - thing that's accessible from every part of the code
+
+var a = 'hello world';
+
+function b() {
+    
+}
