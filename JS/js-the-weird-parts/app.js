af0008a38595574931a66ced7cce9604b7266e24
Kacper Walter <kacper.walter@netguru.com>
2022-05-31 17:43:47 +0200

    start

--- JS/js-the-weird-parts/app.js
+++ JS/js-the-weird-parts/app.js
@@ -5,17 +5,31 @@
 
 
 // variable envoirements (scope)
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
+
+// function a() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   b();
+// }
+
+// var myVar = 1;
+// console.log(myVar)
+// a();
+
+
+// THE SCOPE CHAIN
 function b() {
-  var myVar;
   console.log(myVar);
 }
 
 function a() {
   var myVar = 2;
-  console.log(myVar);
   b();
 }
 
 var myVar = 1;
-console.log(myVar)
 a();
