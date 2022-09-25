fba41af35449085c61a6a48fbb2b4f40b3abdffe
Kacper Walter <kacper.walter@netguru.com>
2022-09-25 16:28:23 +0200

    back project to beggining

--- Projects/minecraft-react-threejs/src/components/FPV.js
+++
@@ -1,8 +0,0 @@
-import { PointerLockControls } from "@react-three/drei"
-import { useThree } from "@react-three/fiber"
-
-export const FPV = () => {
-	const { camera, gl } = useThree()
-
-	return (<PointerLockControls args={[camera, gl.domElement]} />)
-}
