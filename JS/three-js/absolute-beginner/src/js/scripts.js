961389fb882f2f2b94669ad64c060ac8f28b3023
Kacper Walter <kacper.walter@netguru.com>
2022-09-25 18:49:22 +0200

    add orbiting camera

--- JS/three-js/absolute-beginner/src/js/scripts.js
+++ JS/three-js/absolute-beginner/src/js/scripts.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -15,5 +16,32 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-// link renderer to scene and camera
-renderer.render(scene, camera);
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+// 3D coordinates 
+const axesHelper = new THREE.AxesHelper(3);
+
+scene.add(axesHelper);
+
+// camera.position.z = 5;
+// camera.position.y = 2;
+
+// (x, y, z)
+camera.position.set(0, 2, 5);
+// call the method everytime we move the camera
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+
+scene.add(box);
+
+// geometric transofmation 
+function animate() {
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
+  renderer.render(scene, camera); 
+}
+
+renderer.setAnimationLoop(animate);
