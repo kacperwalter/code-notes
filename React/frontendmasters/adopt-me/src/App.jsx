133367b7a3694000cb8c440a9c49a31ab11c70e6
Kacper Walter <52107523+kacperwalter@users.noreply.github.com>
2022-12-18 19:14:39 +0100

    Fem react day 2 (#171)

---
+++ React/frontendmasters/adopt-me/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { className: "first-animal" }, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { className: "test-class" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havaneese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Pepper",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Doink",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
