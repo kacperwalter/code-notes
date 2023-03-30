547148ec00c725a774e8a2c797a129f850004495
kacperwalter <kacwal@poczta.fm>
2023-03-30 21:08:18 +0200

    work for today

--- React/frontendmasters/adopt-me/src/App.jsx
+++ React/frontendmasters/adopt-me/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import { createRoot } from 'react-dom/client';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  const addNumbers = (a, b) => a + b;
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havaneese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
-      <Pet name="Scooby" animal="Dog" breed={addNumbers(2, 5)} />
+      <SearchParams />
     </div>
   );
 };
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
---
+++ React/frontendmasters/adopt-me/src/SearchParams.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const SearchParams = () => {
+  // useState means default value of some state
+  const [location, setLocation] = useState('');
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
