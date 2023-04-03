9d34f17b3ee9db76784145de5b0fa0d14877ba22
kacperwalter <kacwal@poczta.fm>
2023-04-03 19:14:11 +0200

    more logic

--- React/frontendmasters/adopt-me/src/SearchParams.jsx
+++ React/frontendmasters/adopt-me/src/SearchParams.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Pet from './Pet';
 
 const ANIMIALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+const breeds = [];
 
 const SearchParams = () => {
   // useState means default value of some state
   const [location, setLocation] = useState('');
   const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const [pets, setPets] = useState([]);
+
+  // everytime animal changes, useEffect will run
+  useEffect(() => {
+    requestPets();
+  }, [animal])
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -26,6 +44,7 @@ const SearchParams = () => {
             value={animal}
             onChange={(e) => {
               setAnimal(e.target.value);
+              setBreed('');
             }}
           >
             <option />
@@ -34,8 +53,30 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            disabled={breeds.length === 0}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
+      {
+        pets.map(pet => (
+          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+        ))
+      }
     </div>
   );
 };
