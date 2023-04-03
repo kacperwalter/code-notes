8f737b03934aec42779bae41488b6d554434e96a
kacperwalter <kacwal@poczta.fm>
2023-04-03 17:11:38 +0200

    add dropdown

--- React/frontendmasters/adopt-me/src/SearchParams.jsx
+++ React/frontendmasters/adopt-me/src/SearchParams.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 
+const ANIMIALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+
 const SearchParams = () => {
   // useState means default value of some state
   const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
 
   return (
     <div className="search-params">
@@ -16,9 +19,24 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMIALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
-    </div>
+    </div >
   );
 };
 
