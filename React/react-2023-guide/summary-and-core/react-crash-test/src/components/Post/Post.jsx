03c1ca21c6c13a5f0f5146ee80aa07106ba1d31f
kacperwalter <kacwal@poczta.fm>
2023-08-25 15:28:05 +0200

    Add new things

--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/Post/Post.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/Post/Post.jsx
@@ -2,10 +2,10 @@ import styles from './Post.module.css'
 
 const Post = (props) => {
   return (
-    <div className={styles.post}>
+    <li className={styles.post}>
       <p className={styles.author}>{props.author}</p>
       <p className={styles.body}>{props.body}</p>
-    </div>
+    </li>
   );
 }
 
--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/PostsList/PostsList.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/PostsList/PostsList.jsx
@@ -3,10 +3,10 @@ import styles from './PostsList.module.css';
 
 const PostsList = () => {
   return (
-    <>
+    <ul className={styles.posts}>
       <Post author="Kacper" body="React.js is awesome" />
       <Post author="Krzysztof" body="check out React.js" />
-    </>
+    </ul>
   )
 }
 
