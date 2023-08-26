2230c5044ce536931ff7571d66cee5f1e37f0e37
kacperwalter <kacwal@poczta.fm>
2023-08-26 15:33:59 +0200

    Add more code

--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/NewPost/NewPost.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/NewPost/NewPost.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import styles from './NewPost.module.css';
 
 const NewPost = () => {
+  // const stateData = useState(''); // initial value
+  // stateData[0] // current value
+  // stateData[1] // state updating function
+
+  const [ enteredBody, setEnteredBody ] = useState('');
+  
   const changeBodyHandler = (event) => {
-    console.log(event.target.value)
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -11,6 +18,7 @@ const NewPost = () => {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
