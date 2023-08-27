debbb71819f7b8e1a42632a3bc9de209a661542f
kacperwalter <kacwal@poczta.fm>
2023-08-27 16:04:46 +0200

    Learn how to deal with state

--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/NewPost/NewPost.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/NewPost/NewPost.jsx
@@ -1,29 +1,39 @@
 import { useState } from 'react';
 import styles from './NewPost.module.css';
 
-const NewPost = () => {
-  // const stateData = useState(''); // initial value
-  // stateData[0] // current value
-  // stateData[1] // state updating function
-
-  const [ enteredBody, setEnteredBody ] = useState('');
-  
-  const changeBodyHandler = (event) => {
-    setEnteredBody(event.target.value);
+const PostSubmit = () => {
+  const submitPost = () => {
+    console.log("form sent ")
   }
 
   return (
-    <form className={styles.form}>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
-      </p>
-      <p>{enteredBody}</p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
-      </p>
-    </form>
+    <button onClick={submitPost}>
+      I dont do anything
+    </button>
+  )
+}
+
+const NewPost = () => {
+  const [ enteredBody, setEnteredBody ] = useState('');
+
+  const changeBodyHandler = (event) => {
+    setEnteredBody(event.target.value)
+  }
+
+  return (
+    <>
+      <form className={styles.form}>
+        <p>
+          <label htmlFor="body">Text</label>
+          <textarea id="body" required rows={3} onChange={changeBodyHandler}/> 
+        </p>
+        <p>{enteredBody}</p>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" required />
+        </p>
+      </form>
+    </>
   );
 }
 
