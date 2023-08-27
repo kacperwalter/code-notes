78eadbef58821e5a28fcf74e6f2af439f8a36b14
kacperwalter <kacwal@poczta.fm>
2023-08-27 16:27:10 +0200

    Lifting state up

--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/NewPost/NewPost.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/NewPost/NewPost.jsx
@@ -1,36 +1,16 @@
-import { useState } from 'react';
 import styles from './NewPost.module.css';
 
-const PostSubmit = () => {
-  const submitPost = () => {
-    console.log("form sent ")
-  }
-
-  return (
-    <button onClick={submitPost}>
-      I dont do anything
-    </button>
-  )
-}
-
-const NewPost = () => {
-  const [ enteredBody, setEnteredBody ] = useState('');
-
-  const changeBodyHandler = (event) => {
-    setEnteredBody(event.target.value)
-  }
-
+const NewPost = (props) => {
   return (
     <>
       <form className={styles.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={changeBodyHandler}/> 
+          <textarea id="body" required rows={3} onChange={props.onBodyChange}/> 
         </p>
-        <p>{enteredBody}</p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input type="text" id="name" required />
+          <input type="text" id="name" required onChange={props.onAuthorChange}/>
         </p>
       </form>
     </>
--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/PostsList/PostsList.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/PostsList/PostsList.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+
 import Post from '../Post/Post';
 import NewPost from '../NewPost/NewPost';
 import styles from './PostsList.module.css';
 
 const PostsList = () => {
+  const [ enteredBody, setEnteredBody ] = useState('');
+  const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value)
+  }
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value)
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
       <ul className={styles.posts}>
-        <Post author="Kacper" body="React.js is awesome" />
-        <Post author="Krzysztof" body="check out React.js" />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Krzysztof" body='' />
       </ul>
     </>
   )
