d66642c1cb5ea6065533283022e6a3d3fdf878b5
kacperwalter <kacwal@poczta.fm>
2023-08-28 12:55:48 +0200

    Add more exercise

--- JS/the-advanced-concepts/exercises.js
+++ JS/the-advanced-concepts/exercises.js
@@ -39,9 +39,9 @@ function getCount([...str]) {
 function getMiddle(string) {
   return string.length % 2 === 0
     ? string.substring(
-        Math.floor(string.length / 2 - 1),
-        Math.floor(string.length / 2 - 1)
-      ) + 2
+      Math.floor(string.length / 2 - 1),
+      Math.floor(string.length / 2 - 1)
+    ) + 2
     : string.substring(string.length / 2, string.length / 2 + 1);
 }
 
@@ -70,9 +70,9 @@ function isValidWalk(walk) {
 // console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']));
 
 function accum([...string]) {
-	string.map((el, index) => {
+  string.map((el, index) => {
     console.log(el.repeat(index + 1));
-}); 
+  });
 }
 
 // accum("ZpglnRxqenU")
@@ -171,7 +171,7 @@ function digitalRoot(n) {
 
 const likes = names => {
   names = names || [];
-  switch(names.length) {
+  switch (names.length) {
     case 0:
       return 'no one likes this'
     case 1:
@@ -191,7 +191,7 @@ const likes = names => {
 // likes(['Alex', 'Jacob', 'Mark', 'Max', 'Max', 'Max', 'Max'])
 
 
-function high(string){
+function high(string) {
   const alphabet = Array.from(Array(26)).map((e, i) => i + 65).map((letter) => String.fromCharCode(letter));
   let highestWord = '';
   let currentBiggestScore = 0;
@@ -246,7 +246,7 @@ function high(string){
 //   // console.log(number.toString(2));
 //   return number.toString(2).split('').reduce((sum, currentValue) => sum + parseInt(currentValue), 0)
 // }
-  
+
 const countBits = number => number.toString(2).split('').reduce((sum, currentValue) => sum + parseInt(currentValue), 0);
 
 
@@ -287,7 +287,7 @@ const alphabetPosition = ([...text]) => {
 
 // function isValidIP(str) {
 //   IP = str.split('.')
-  
+
 //   if (IP.length === 4) { // TODO oneline if
 //     IP.forEach(element => {
 //       if (element.match(/[a-z]/i) === null) { // TODO oneline if
@@ -301,7 +301,7 @@ const alphabetPosition = ([...text]) => {
 
 // function isValidIP(str) {
 //   IP = str.split('.')
-  
+
 //   if (IP.length === 4) {
 //     IP.forEach(element => {
 //       if (element.match(/[a-z]/i) != null) return;
@@ -316,7 +316,7 @@ const alphabetPosition = ([...text]) => {
 
 const isValidIP = str => {
   IP = str.split('.')
-  
+
   if (IP.length === 4) {
     for (const element of IP) {
       if (element.match(/[a-z]/i) != null) return false;
@@ -339,3 +339,130 @@ const isValidIP = str => {
 const sumTwoSmallestNumbers = (numbers) => numbers.sort((a, b) => a - b)[0] + numbers.sort((a, b) => a - b)[1]
 
 // console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
+
+// The global variable
+const watchList = [
+  {
+    "Title": "Inception",
+    "Year": "2010",
+    "Rated": "PG-13",
+    "Released": "16 Jul 2010",
+    "Runtime": "148 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Christopher Nolan",
+    "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+    "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    "Language": "English, Japanese, French",
+    "Country": "USA, UK",
+    "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    "Metascore": "74",
+    "imdbRating": "8.8",
+    "imdbVotes": "1,446,708",
+    "imdbID": "tt1375666",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Interstellar",
+    "Year": "2014",
+    "Rated": "PG-13",
+    "Released": "07 Nov 2014",
+    "Runtime": "169 min",
+    "Genre": "Adventure, Drama, Sci-Fi",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan, Christopher Nolan",
+    "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    "Language": "English",
+    "Country": "USA, UK",
+    "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    "Metascore": "74",
+    "imdbRating": "8.6",
+    "imdbVotes": "910,366",
+    "imdbID": "tt0816692",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "The Dark Knight",
+    "Year": "2008",
+    "Rated": "PG-13",
+    "Released": "18 Jul 2008",
+    "Runtime": "152 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+    "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+    "Language": "English, Mandarin",
+    "Country": "USA, UK",
+    "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+    "Metascore": "82",
+    "imdbRating": "9.0",
+    "imdbVotes": "1,652,832",
+    "imdbID": "tt0468569",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "Rated": "PG-13",
+    "Released": "15 Jun 2005",
+    "Runtime": "140 min",
+    "Genre": "Action, Adventure",
+    "Director": "Christopher Nolan",
+    "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+    "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+    "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+    "Language": "English, Urdu, Mandarin",
+    "Country": "USA, UK",
+    "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+    "Metascore": "70",
+    "imdbRating": "8.3",
+    "imdbVotes": "972,584",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Avatar",
+    "Year": "2009",
+    "Rated": "PG-13",
+    "Released": "18 Dec 2009",
+    "Runtime": "162 min",
+    "Genre": "Action, Adventure, Fantasy",
+    "Director": "James Cameron",
+    "Writer": "James Cameron",
+    "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    "Language": "English, Spanish",
+    "Country": "USA, UK",
+    "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    "Metascore": "83",
+    "imdbRating": "7.9",
+    "imdbVotes": "876,575",
+    "imdbID": "tt0499549",
+    "Type": "movie",
+    "Response": "True"
+  }
+];
+
+// function getRating(watchList) {
+//   const result = watchList.map(element => parseFloat(element["imdbRating"])).reduce((sum, current) => sum + current, 0) / watchList.length;
+//   return result
+// }
+
+const getRating = (watchList) => watchList
+  .filter(film => film.Director === "Christopher Nolan")
+  .map(element => Number(element["imdbRating"])).reduce((sum, current) => sum + current, 0)
+  / watchList.filter(film => film.Director === "Christopher Nolan").length
+
+console.log(getRating(watchList));
+// getRating(watchList)
--- React/react-2023-guide/summary-and-core/react-crash-test/src/App.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
   return (
     <>
       <MainHeader onCreatePost={showModalHandler} />
+      {console.log(Window)}
       <main>
         <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
       </main>
