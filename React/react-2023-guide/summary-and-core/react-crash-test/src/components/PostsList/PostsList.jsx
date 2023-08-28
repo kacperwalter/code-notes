58836e203e0d8567200a00fd3afdcd68cc961d57
kacperwalter <kacwal@poczta.fm>
2023-08-28 14:49:08 +0200

    Add comment

--- React/react-2023-guide/summary-and-core/react-crash-test/src/components/PostsList/PostsList.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/components/PostsList/PostsList.jsx
@@ -14,6 +14,7 @@ const PostsList = ({ isPosting, onStopPosting }) => {
       setIsFetching(true);
       const response = await fetch('http://localhost:8080/posts');
       const resData = await response.json();
+      !response.ok ? 'test' : 'test'; // TODO handle not ok response
       setPosts(resData.posts);
       setIsFetching(false);
     }
