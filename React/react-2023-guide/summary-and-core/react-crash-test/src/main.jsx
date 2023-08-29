33243f452daa9fe23ce117ba0e7bdfadabf198d4
kacperwalter <kacwal@poczta.fm>
2023-08-29 13:52:42 +0200

    Add basic routing

--- React/react-2023-guide/summary-and-core/react-crash-test/src/main.jsx
+++ React/react-2023-guide/summary-and-core/react-crash-test/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+
 import App from './App'
+import NewPost from './components/NewPost/NewPost'
 import './index.css'
+
+// list of routers i want to have in project
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-post', element: <NewPost /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
