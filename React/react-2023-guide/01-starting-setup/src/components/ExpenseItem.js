06e79a0c45afed65ec2cd385b498e6fa8b8da631
kacperwalter <kacwal@poczta.fm>
2023-09-01 11:02:28 +0200

    Add some props, pasing props etc

--- React/react-2023-guide/01-starting-setup/src/components/ExpenseItem.js
+++ React/react-2023-guide/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 2, 28);
-  const expenseTile = 'Car Insurance';
-  const expenseAmount = 294.67;
+import ExpenseDate from './ExpenseDate'
 
+function ExpenseItem({ title, amount, date }) {
   return (
     <div className='expense-item'>
-      <div>{expenseDate.getDate()}</div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{expenseTile}</h2>
-        <div className='expense-item__price'>294.67</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{amount}</div>
       </div>
     </div>
   )
