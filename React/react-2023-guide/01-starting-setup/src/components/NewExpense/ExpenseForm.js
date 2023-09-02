93904e0e3a3eaed00c088ac30eafce1ef5919f83
Kacper Walter <kacperwalter@192.168.1.35>
2023-09-02 20:26:46 +0200

    Configured new envoirement on new computer

--- React/react-2023-guide/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ React/react-2023-guide/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,7 @@ const ExpenseForm = () => {
           <input type='number' min="0.01" step="0.01" />
         </div>
         <div className='new-expense__control'>
-          <label>Date</label>
+          <label>Datee</label>
           <input type='date' min='2019-01-01' max='2022-12-13' />
         </div>
       </div>
