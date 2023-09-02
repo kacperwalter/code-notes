7b269a42817ad10d503f9e54b503d7d03d4f2ad8
kacperwalter <kacwal@poczta.fm>
2023-09-02 17:25:39 +0200

    Add state

--- React/react-2023-guide/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React/react-2023-guide/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,23 @@
+import { useState } from 'react'
+
 import './ExpenseItem.css'
 
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [header, setHeader] = useState(title);
+
+  const clickHandler = () => setHeader("Updated!");
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{header}</h2>
         <div className='expense-item__price'>{amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   )
 }
