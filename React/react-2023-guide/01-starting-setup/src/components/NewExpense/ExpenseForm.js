07c22ba5f49f9689b61ebe877aee56284eb0f306
kacperwalter <kacwal@poczta.fm>
2023-09-02 18:40:47 +0200

    Wip

--- React/react-2023-guide/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ React/react-2023-guide/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,20 @@
+import { useState } from 'react'
+
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnterdTitle] = useState('')
+
+  const titleChangeHandler = event => {
+    setEnterdTitle(event.target.value)
+  }
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
