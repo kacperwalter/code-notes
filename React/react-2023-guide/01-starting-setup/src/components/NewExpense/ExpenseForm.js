4fb1024f71c87734fe1b5412301e2928ba706568
Kacper Walter <kacperwalter@192.168.1.35>
2023-09-03 11:54:21 +0200

    Add more state handling to form

--- React/react-2023-guide/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ React/react-2023-guide/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -3,11 +3,14 @@ import { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnterdTitle] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmout, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
 
-  const titleChangeHandler = event => {
-    setEnterdTitle(event.target.value)
-  }
+  const titleChangeHandler = event => setEnteredTitle(event.target.value)
+  const amountChangeHandler = event => setEnteredAmount(event.target.value)
+  const dateChangeHandler = event => setEnteredDate(event.target.value)
+
 
   return (
     <form>
@@ -18,11 +21,11 @@ const ExpenseForm = () => {
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min="0.01" step="0.01" />
+          <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Datee</label>
-          <input type='date' min='2019-01-01' max='2022-12-13' />
+          <input type='date' min='2019-01-01' max='2022-12-13' onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
