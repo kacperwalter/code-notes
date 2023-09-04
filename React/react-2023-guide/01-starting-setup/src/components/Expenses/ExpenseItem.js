166b72ae508d4a1781f23853ae7bb8cac2d53b20
kacperwalter <kacwal@poczta.fm>
2023-09-04 13:35:04 +0200

    Add more code from course

--- React/react-2023-guide/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React/react-2023-guide/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,15 @@ import Card from '../UI/Card'
 
 const ExpenseItem = ({ title, amount, date }) => {
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>{amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={date} />
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>{amount}</div>
+        </div>
+      </Card>
+    </li>
   )
 }
 
--- React/react-2023-guide/01-starting-setup/src/components/Expenses/Expenses.js
+++ React/react-2023-guide/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './Expenses.css'
 
-import ExpenseItem from "./ExpenseItem"
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
 
 const Expenses = ({ items }) => {
   const [filteredYear, setFilteredYear] = useState('2019')
@@ -12,24 +12,11 @@ const Expenses = ({ items }) => {
 
   const filteredExpenses = items.filter(item => (item.date.getFullYear()).toString() === filteredYear)
 
-  let expensesContent = <p>No expenses found</p>
-
-  if (filteredExpenses > 0) {
-    expensesContent = filteredExpenses.map(item => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      />
-    ))
-  }
-
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   )
