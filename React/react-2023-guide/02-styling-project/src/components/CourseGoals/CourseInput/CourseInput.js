8a392103aea870d08da08dc2b32fb8612cfad510
kacperwalter <kacwal@poczta.fm>
2023-09-05 12:47:51 +0200

    Add files from finished course section

--- React/react-2023-guide/02-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ React/react-2023-guide/02-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from '../../UI/Button/Button'
+
+import styles from './CourseInput.module.css'
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    setEnteredValue(event.target.value)
+  }
 
   const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    }
+    props.onAddGoal(enteredValue)
+  }
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
-  );
-};
+  )
+}
 
-export default CourseInput;
+export default CourseInput
