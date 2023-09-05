e89f02c831d8f8472a0b969eb8e2cee43729e86e
kacperwalter <kacwal@poczta.fm>
2023-09-05 19:49:30 +0200

    Add files

--- TS/first-project/js-only.js
+++ TS/first-project/js-only.js
@@ -1,11 +1,14 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-
+var button = document.querySelector("button");
+// !as HTMLInputElement  ->  we are shure that we will get that element
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+// types must be numbers
 function add(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
-
-button.addEventListener("click", function() {
-  console.log(add(input1.value, input2.value));
+button.addEventListener("click", function () {
+    // + means that we change type to number
+    console.log(add(+input1.value, +input2.value));
 });
+var someElements = [1, 2, 3];
+var filteredElements = someElements.filter(function (element) { return element === 3; });
