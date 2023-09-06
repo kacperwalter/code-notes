ca3a4be4c659c27a2e2da014cc6180bc851ea67a
kacperwalter <kacwal@poczta.fm>
2023-09-06 13:58:34 +0200

    Add object state handling

--- React/react-2023-guide/Practice-projects/add-user/src/App.jsx
+++ React/react-2023-guide/Practice-projects/add-user/src/App.jsx
@@ -6,13 +6,15 @@ import AddUserForm from "./components/AddUserForm/AddUserForm"
 import UsersList from "./components/UsersList/UsersList"
 
 function App() {
-  const [userData, setUserData] = useState('')
+  const [userData, setUserData] = useState([])
 
-  // const setUserDataHandler = event => setUserData(event)
-  const setUserDataHandler = event => console.log(event)
+  const setUserDataHandler = data => setUserData(prevData => {
+    return [data, ...prevData]
+  })
 
   return (
     <Wrapper>
+      <h1>A fucking HTML website</h1>
       <AddUserForm onSaveForm={setUserDataHandler}/>
       <UsersList />
     </Wrapper>
