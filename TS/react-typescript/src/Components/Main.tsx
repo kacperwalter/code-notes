8d006df2ee121441b1b50b6d9d2e975dcfbf44b6
kacperwalter <kacwal@poczta.fm>
2023-09-06 10:26:36 +0200

    Wip

--- TS/react-typescript/src/Components/Main.tsx
+++ TS/react-typescript/src/Components/Main.tsx
@@ -1,13 +1,23 @@
-import { FC  } from "react";
+import { FC } from "react";
+import { useState } from "react";
 
 interface MainProps {
-  first: boolean;
-  second: string;
+  first?: boolean;
+  second?: string;
 }
 
-const Main:FC<MainProps> = (props) => {
+const Main: FC<MainProps> = ({ first, second }) => {
+  const [value, setValue] = useState(0)
+
+  const someHelperFunction = (event:Object) => {
+    setValue(event)
+  }
+
   return (
-    <div>{props.second}</div>
+    <>
+      <div>{first}</div>
+      <div>{second}</div>
+    </>
   )
 }
 
