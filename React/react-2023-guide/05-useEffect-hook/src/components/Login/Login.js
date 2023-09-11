d02aa06b0a0c2e453113f4aabd662412e3e3bcd1
kacperwalter <kacwal@poczta.fm>
2023-09-11 20:43:49 +0200

    Add useEffect() explaination

--- React/react-2023-guide/05-useEffect-hook/src/components/Login/Login.js
+++ React/react-2023-guide/05-useEffect-hook/src/components/Login/Login.js
@@ -12,17 +12,26 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    )
+    console.log('effect running')
+  }, [enteredPassword])
+
+  useEffect(() => {
+    const identifiier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      )
+    }, 500)
+
+    // Cleanup function - it runs after first and before every new execution of useEffect()
+    // https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599220#content
+    // very important in form validation
+    return () => {
+      clearTimeout(identifiier);
+    }
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
