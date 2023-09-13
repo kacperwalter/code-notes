170dd0f3b1d257257f9b67ffc9d0af463985421f
kacperwalter <kacwal@poczta.fm>
2023-09-13 14:06:56 +0200

    Wip bo nie ogarniam

--- React/react-2023-guide/05-useEffect-hook/src/components/Login/Login.js
+++ React/react-2023-guide/05-useEffect-hook/src/components/Login/Login.js
@@ -1,49 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    
+  }
+  return { value: '', isValid: false }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: false });
+
   useEffect(() => {
     console.log('effect running')
-  }, [enteredPassword])
 
-  useEffect(() => {
-    const identifiier = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      )
-    }, 500)
-
-    // Cleanup function - it runs after first and before every new execution of useEffect()
-    // https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599220#content
-    // very important in form validation
     return () => {
-      clearTimeout(identifiier);
+      console.log('cleanup')
     }
-  }, [enteredEmail, enteredPassword])
+  }, [enteredPassword])``
+
+  // useEffect(() => {
+  //   const identifiier = setTimeout(() => {
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     )
+  //   }, 500)
+
+  //   // Cleanup function - it runs after first and before every new execution of useEffect()
+  //   // https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599220#content
+  //   // very important in form validation
+  //   return () => {
+  //     clearTimeout(identifiier);
+  //   }
+  // }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    )
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+      emailState.isValid && event.target.value.trim().length > 6
     );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(emailState.value.includes('@'));
   };
 
   const validatePasswordHandler = () => {
@@ -52,21 +69,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${emailState.valid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
@@ -79,7 +96,7 @@ const Login = (props) => {
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={emailState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
