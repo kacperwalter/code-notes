8c042b69b6cf060a1e695f4b5d06e752bb6cbd2d
kacperwalter <kacwal@poczta.fm>
2023-09-15 21:32:13 +0200

    Add useEffect practice

--- React/react-2023-guide/Practice-projects/useEffect-learning/src/main.jsx
+++ React/react-2023-guide/Practice-projects/useEffect-learning/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import UseEffectAdvanced from './UseEffectCleanup.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <UseEffectAdvanced />
     <App />
   </React.StrictMode>,
 )
