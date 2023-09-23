830cb530bda2ddc3ae94b711c2a21c4ad5d09454
kacperwalter <kacwal@poczta.fm>
2023-09-23 13:31:19 +0200

    Add useContext refactoring

--- React/react-2023-guide/06-usereducer-starting-project/src/components/Home/Home.js
+++ React/react-2023-guide/06-usereducer-starting-project/src/components/Home/Home.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import { useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import AuthContext from '../../context/auth-context';
 
 const Home = (props) => {
+  const authCtx = useContext(AuthContext)
+
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
--- React/react-2023-guide/06-usereducer-starting-project/src/components/Login/Login.js
+++ React/react-2023-guide/06-usereducer-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
 
 const emailReducer = (state, action) => {
   switch (action.type) {
@@ -65,6 +66,8 @@ const Login = (props) => {
     };
   }, [emailIsValid, passwordIsValid]);
 
+  const authCtx = useContext(AuthContext)
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
 
@@ -91,7 +94,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
