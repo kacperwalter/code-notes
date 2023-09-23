25e1b74a9bc3c42e62148cd1f520ad91c38e7f4d
kacperwalter <kacwal@poczta.fm>
2023-09-23 12:53:36 +0200

    Add useContext handling

--- React/react-2023-guide/06-usereducer-starting-project/src/components/MainHeader/Navigation.js
+++ React/react-2023-guide/06-usereducer-starting-project/src/components/MainHeader/Navigation.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../context/auth-context';
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext)
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
