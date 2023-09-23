7747c0cf4d2486ea5d2b6013c25d0bb871ac34eb
kacperwalter <kacwal@poczta.fm>
2023-09-23 12:24:39 +0200

    Add context api handling but without hook

--- React/react-2023-guide/06-usereducer-starting-project/src/App.js
+++ React/react-2023-guide/06-usereducer-starting-project/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import AuthContext from './context/auth-context';
+
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -28,13 +30,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
