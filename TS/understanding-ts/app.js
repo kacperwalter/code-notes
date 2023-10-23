78456fb59a559920da64d5b0f9ed425dcc41fd61
kacperwalter <kacwal@poczta.fm>
2023-10-23 09:30:23 +0200

    Wip

--- TS/understanding-ts/app.js
+++ TS/understanding-ts/app.js
@@ -1,13 +1,16 @@
 // union type - for combining types
-var combine = function (input1, input2) {
+// 3rd parameter is  a literal type
+var combine = function (input1, input2, resultType) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') { // we have to work with the strict type of input
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') { // we have to work with the strict type of input
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 };
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
--- TS/understanding-ts/app.ts
+++ TS/understanding-ts/app.ts
@@ -1,14 +1,18 @@
 // union type - for combining types
 
-const combine = (input1: number | string, input2: number | string) => {
+// 3rd parameter is  a literal type
+const combine = (input1: number | string, input2: number | string, resultType: string) => {
   let result
-  if (typeof input1 === 'number' && typeof input2 === 'number') { // we have to work with the strict type of input
-    result = input1 + input2
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') { // we have to work with the strict type of input
+    result = +input1 + +input2
   } else {
     result = input1.toString() + input2.toString()
   }
   return result
 }
 
-const combinedAges = combine(30, 26)
+const combinedAges = combine(30, 26, 'as-number')
 console.log(combinedAges)
+
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges)
