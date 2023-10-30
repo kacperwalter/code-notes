c651790dd0c4c3db8786fa25adba229c41a25a5c
kacperwalter <kacwal@poczta.fm>
2023-10-30 19:15:22 +0100

    Add arguments keyword exercises

--- JS/the-advanced-concepts/lectures.js
+++ JS/the-advanced-concepts/lectures.js
@@ -1,7 +1,7 @@
 // exercise 1 - hoisting
 // hoisting happens in every execution context
 // hoisting is the process of putting all the variable declarations and function declarations into memory during the compile phase and assign undefined to all variables
-a();
+// a();
 
 function a() {
   console.log("hi");
@@ -11,6 +11,7 @@ function a() {
 function a() {
   console.log("bye");
 }
+
 
 // exercise 2 
 var favouriteFood = "grapes";
@@ -24,10 +25,10 @@ var foodThoughts = function () {
   console.log("New favourite food: " + favouriteFood);
 }
 
-foodThoughts();
+// foodThoughts();
+
 
 // exercise 3
-
 function bigBrother(){
   function littleBrother() {
     return 'it is me!';
@@ -39,4 +40,22 @@ function bigBrother(){
 }
 
 // Before running this code, what do you think the output is?
-console.log(bigBrother());
+// console.log(bigBrother());
+
+
+
+// function invocation
+function marry(person1, person2) {
+  // arguments keyword is an array-like object that contains all the arguments passed to the function
+  console.log("arguments", arguments); // but we shouldnt use arguments keyword like this in ES6
+  console.log(...arguments); // this is the way to use arguments keyword in ES6
+}
+
+function marry2(...args) {
+  console.log("arguments", args);
+  console.log(...args);
+  const [person1, person2] = args; // this is the way to destructure arguments keyword in ES6
+}
+
+// marry("Tim", "Tina");
+marry2("Tim", "Tina");
