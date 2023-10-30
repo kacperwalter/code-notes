408e2fbdc8c91fa9f3f31c2e3ca94645898235b4
kacperwalter <kacwal@poczta.fm>
2023-10-30 18:51:37 +0100

    Add hoisting exercises

--- JS/the-advanced-concepts/hoisting.js
+++ JS/the-advanced-concepts/hoisting.js
@@ -1,4 +1,6 @@
 // exercise 1 - hoisting
+// hoisting happens in every execution context
+// hoisting is the process of putting all the variable declarations and function declarations into memory during the compile phase and assign undefined to all variables
 a();
 
 function a() {
@@ -9,3 +11,32 @@ function a() {
 function a() {
   console.log("bye");
 }
+
+// exercise 2 
+var favouriteFood = "grapes";
+
+var foodThoughts = function () {
+  // hoisting happens in every execution context (and thats why favouriteFood is undefined - because new var declaration was hoisted inside the function when new execution context was created)
+  console.log("Original favourite food: " + favouriteFood);
+
+  var favouriteFood = "sushi";
+
+  console.log("New favourite food: " + favouriteFood);
+}
+
+foodThoughts();
+
+// exercise 3
+
+function bigBrother(){
+  function littleBrother() {
+    return 'it is me!';
+  }
+  return littleBrother();
+  function littleBrother() { // will run this function because it was hoisted to the top of the execution context
+    return 'no me!';
+  }
+}
+
+// Before running this code, what do you think the output is?
+console.log(bigBrother());
